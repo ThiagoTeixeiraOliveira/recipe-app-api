8fd96300dd5bbcf03455bb396404919d0c2d36e0
Thiago Horizonti <thiagohorizonti@MacBook-Air-de-Thiago-19.local>
2023-03-26 15:14:32 -0300

    Added and implemented User Model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,44 @@
-from django.db import models  # noqa
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    # extra_fields can provide keyword arguments, giving flexibility
+    # as it means we dont need to update create_user method
+    # because it automatically provides them when we call the method
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+"""
+AbstractBaseUser -> Contains the functionality for the authentication
+                    system(but not any fields)
+PermissionsMixin -> Contains the functionality for the permissions feature
+                    of Django and contains fields that are needed for this
+                    permissions feature
+"""
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    # Defines the field used for authentication
+    # (how we replace default field "username")
+    USERNAME_FIELD = 'email'
