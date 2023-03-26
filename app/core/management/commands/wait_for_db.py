e8dfe4a8ab1e24241be0bd9bf475ab13710d1584
Thiago Horizonti <thiagohorizonti@MacBook-Air-de-Thiago-19.local>
2023-03-26 14:29:00 -0300

    Configured Docker compose and wait for db

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,30 @@
+"""
+Django command to wait for the database to be available.
+"""
+import time
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for database."""
+
+    def handle(self, *args, **options):
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database...')
+        # Assuming that the db is not up until we know that it is
+        db_up = False
+        while db_up is False:
+            try:
+                # If the database isn't ready when this is called,
+                # it throws Pycopg2OpError or OperationalError
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,45 @@
+"""
+Test custom Django management commands.
+"""
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+# Those patches are the arguments from the class functions(i.e patched_check)
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test commands."""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready"""
+        # When mocking an object, we simply return true
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError"""
+        # As we want to raise exceptions that would be
+        # raised if the database wasn't ready, we use the side_effect
+        # because it allows us to pass in various different items
+        # that get handled differently depending on the time.
+        # (if we pass an exception, mocking library knows
+        # it should raise the exception)
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+        # In this case, we are saying that the first two times we call
+        # the mocked method, we raise the psycopg2error, and the next 3 times
+        # we raise an operationalError. After those 5 calls, it returns True
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
